30704d19058d2e05becdbea42ca7221e37ea94eb
karthickAppu <karthick1398@gmail.com>
2024-01-28 18:40:22 +0530

    Adding Server folder

---
+++ Server/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const app = express();
+
+
+//middleware
+app.use((req,res,next) => {
+    console.log("path " + req.path + "method " + req.method);
+    next();
+});
+
+
+app.get('/',(req,res)=>{
+    res.send("Motor Insurance");
+});
+
+//db connection
+mongoose.connect(process.env.MANGO_URI)
+.then(() => {
+    app.listen(process.env.PORT,()=>{
+        console.log("DB is connected sucessfully and Listening to port " + process.env.PORT);
+});
+}).catch((error) => console.log(error));
